0f13cf2b9c43dc0491118243d5d743fdd09d823e
Shiho Onitsuka <shiho.onitsuka@robinson.com>
2023-07-22 23:12:34 +0200

    add whiteboard new feature & css

--- whiteboard.js
+++ whiteboard.js
@@ -2,10 +2,94 @@ const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 let drawing = false;
 let mode = 'draw'; // can be 'draw' or 'erase'
+let textPosition = 20; // starting Y position for text
+
+let addingText = false; // whether we're currently adding text
+let currentTextInput = null; // to store current text input field if any
+
+let textObjects = []; // to store all texts and their positions
+let erasingText = false; // whether we're currently erasing text
+
+
+document.getElementById('textButton').addEventListener('click', function() {
+    // remove existing text input field if any
+    if (currentTextInput) {
+        currentTextInput.parentNode.removeChild(currentTextInput);
+        currentTextInput = null;
+    }
+    addingText = true;
+});
+
+canvas.addEventListener('click', function(e) {
+    if (erasingText) {
+        // check if we clicked on a text
+        for (let i = textObjects.length - 1; i >= 0; i--) {
+            const textObject = textObjects[i];
+            const textWidth = context.measureText(textObject.text).width;
+            if (e.clientX >= textObject.x && e.clientX <= textObject.x + textWidth
+                && e.clientY >= textObject.y - 10 && e.clientY <= textObject.y) {
+                // we clicked on a text, remove it
+                textObjects.splice(i, 1);
+                // redraw the canvas
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                for (const textObject of textObjects) {
+                    context.fillText(textObject.text, textObject.x, textObject.y);
+                }
+                saveCanvas();
+                return;
+            }
+        }
+    }
+    if (!addingText) return;
+    addingText = false;
+    // create new text input field
+    const textInput = document.createElement('input');
+    textInput.type = 'text';
+    textInput.style.position = 'absolute';
+    textInput.style.left = (e.clientX - canvas.offsetLeft) + 'px';
+    textInput.style.top = (e.clientY - canvas.offsetTop) + 'px';
+    document.body.appendChild(textInput);
+    textInput.focus();
+    currentTextInput = textInput;
+    textInput.addEventListener('input', function() {
+        // adjust the size of the input field based on the text length
+        textInput.size = Math.max(1, textInput.value.length); // at least 
+    });
+    textInput.addEventListener('keydown', function(e) {
+        if (e.key === 'Enter') {
+            const text = textInput.value;
+            if (text !== '') {
+                context.fillText(text, e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+                saveCanvas();
+            }
+            // remove the text input field
+            textInput.parentNode.removeChild(textInput);
+            currentTextInput = null;
+            // add this text to our array
+            textObjects.push({text: text, x: e.clientX - canvas.offsetLeft, y: e.clientY - canvas.offsetTop});
+        
+        }
+    });
+});
+
 
 // Set canvas size
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+textInput.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') {
+        const text = textInput.value;
+        if (text !== '') {
+            context.fillText(text, 50, textPosition);
+            textPosition += 30; // move to next line for next text
+            saveCanvas();
+        }
+        // remove the text input field
+        textInput.parentNode.removeChild(textInput);
+        currentTextInput = null;
+    }
+});
 
 function saveCanvas() {
     localStorage.setItem('canvasData', canvas.toDataURL());
@@ -49,14 +133,20 @@ document.getElementById('drawButton').addEventListener('click', function() {
     mode = 'draw';
 });
 
+
 document.getElementById('eraseButton').addEventListener('click', function() {
     mode = 'erase';
+    erasingText = true; // enable erasing text
 });
+
 
 document.getElementById('clearButton').addEventListener('click', function() {
     context.clearRect(0, 0, canvas.width, canvas.height);
+    textObjects = []; // remove all texts
     saveCanvas();
 });
+
+document.getElementById('saveButton').addEventListener('click', saveCanvas);
 
 let color = 'black'; // default color
 
